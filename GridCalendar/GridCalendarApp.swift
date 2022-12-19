2a98e5fe97525fbd92fc729da00fff043bc0a8e0
Skyawesome73 <skyawesome73@gmail.com>
2022-12-19 11:27:57 -0600

    Initial Commit

---
+++ GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by Nicholas Puig on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
